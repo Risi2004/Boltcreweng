f5e8d052ac43e374eecade7bb8cb5635a3a2a195
Risi2004 <risikesanjegatheesan@gmail.com>
2025-11-28 09:10:08 +0530

    updated navbar + top to page button

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-function dropDown(){
-    const dropDown = document.getElementById('dropdown');
+// Shop Drop Down
+
+function toggleDropDown() {
+    const dropDownIcon = document.getElementById('dropdown');
+    const dropUpIcon = document.getElementById('dropup');
     const shopDropDown = document.querySelector('.navbar__shop__dropdown');
-    const dropUp = document.getElementById('dropup');
-    
-    shopDropDown.style.display = 'flex';
-    dropDown.style.display = 'none';
-    dropUp.style.display = 'inline';
 
-    dropUp.onclick = function(){
+    if (shopDropDown.style.display === 'flex') {
         shopDropDown.style.display = 'none';
-        dropUp.style.display = 'none';
-        dropDown.style.display = 'inline';
+        dropUpIcon.style.display = 'none';
+        dropDownIcon.style.display = 'inline';
+    } else {
+        shopDropDown.style.display = 'flex';
+        dropUpIcon.style.display = 'inline';
+        dropDownIcon.style.display = 'none';
     }
-
 }
+
+// Go To Top Button Appear on Scroll
+
+const goToTopButton = document.querySelector('.go__to__top__button');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        goToTopButton.classList.add('active');
+    }
+    else {
+        goToTopButton.classList.remove('active');
+    }
+})
+
+// Smooth Scroll to Top on Button Click
+
+goToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
