09f073605bdd23b4addc71b3e1d6eb1abd3d28b4
Risi2004 <risikesanjegatheesan@gmail.com>
2025-11-28 12:44:39 +0530

    responsive navbar updated

--- index.js
+++ index.js
@@ -37,3 +37,45 @@ goToTopButton.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
+
+// Responsive Side Bar
+
+const sideBar = document.querySelector('.responsive__side__navbar');
+const menuBar = document.querySelector('.responsive__navbar__s1 img');
+const closeBar = document.querySelector('.responsive__side__navbar__close');
+const allLinks = document.querySelectorAll('.responsive__side__navbar a');
+
+menuBar.addEventListener('click', () => {
+    sideBar.style.left = '0';
+    document.body.style.overflow = 'hidden';
+});
+
+closeBar.addEventListener('click', () => {
+    sideBar.style.left = '-80%';
+    document.body.style.overflow = 'auto';
+});
+
+allLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sideBar.style.left = '-80%';
+        document.body.style.overflow = 'auto';
+    });
+});
+
+// Responsive Shop Drop Down
+
+function toggleDropDownTwo() {
+    const responsiveShop = document.querySelector('.side__navbar__sub__s2');
+    const responsiveDropDownIcon = document.getElementById('sidebardropdown');
+    const responsiveDropUpIcon = document.getElementById('sidebardropup');
+
+    if (responsiveShop.style.display === 'flex') {
+        responsiveShop.style.display = 'none';
+        responsiveDropUpIcon.style.display = 'none';
+        responsiveDropDownIcon.style.display = 'inline';
+    } else {
+        responsiveShop.style.display = 'flex';
+        responsiveDropUpIcon.style.display = 'inline';
+        responsiveDropDownIcon.style.display = 'none';
+    } 
+}
